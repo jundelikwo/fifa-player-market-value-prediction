98e8f03e9ca2870d1ef049b7df4f995360ac1f17
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2020-10-19 00:45:34 +0100

    Convert dataset value column to float

--- ml-model.py
+++ ml-model.py
@@ -8,3 +8,24 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('data.csv')
 
+to_stay=[
+    "ID","Name","Age","Nationality","Club","Overall","Potential",
+    "Value","Wage","Real Face"
+]
+
+dataset.drop(dataset.columns.difference(to_stay),axis="columns",inplace=True)
+
+
+dataset.set_index("ID",inplace=True)
+dataset.isnull().sum()
+
+
+# Convert Value column from string to number
+dataset['Value2'] = dataset['Value'].apply(lambda x: x.split('€')[1])
+dataset['Value3'] = dataset['Value2'].apply(
+    lambda x: float(x.split('M')[0])*1000000 
+    if x.split('M').__len__() > 1 else float(x.split('K')[0])*1000
+)
+
+dataset.drop(['Value2', 'Value'], axis="columns",inplace=True)
+dataset.rename(columns={"Value3":"Value"}, inplace=True)
