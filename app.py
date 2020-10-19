7ec1a0c6aa13329fe5a3783c5f2603abccd7d210
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2020-10-19 20:00:49 +0100

    Added predict market value api route

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+import pickle
+
 
 app = Flask(__name__)
+
+model = pickle.load(open("model.pkl","rb"))
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    print(request.json)
+    age = int(request.json.get('Age') or 0)
+    overall = int(request.json.get('Overall') or 0)
+    potential = int(request.json.get('Potential') or 0)
+    wage = int(request.json.get('Wage') or 0)
+    
+    data = [[age, overall, potential, wage]]
+    
+    prediction = model.predict(data)[0]
+    prediction = round(prediction, 2)
+    
+    formatted_value = '€' + str(round(prediction / 1000, 2)) + 'K' if (prediction / 1000) < 1000 else '€' + str(round(prediction / 1000000, 2)) + 'M'
+    
+    result = {'value': prediction, 'formatted_value': formatted_value}
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
